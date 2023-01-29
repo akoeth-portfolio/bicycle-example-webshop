80ad408e3fc169af8d969c8ec2948e09c74ff9d7
dukewellington61 <dukewellington61@googlemail.com>
2023-01-29 19:31:20 +0700

    remove uuid

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,4 +1,4 @@
-import * as uuid from 'uuid'
+// import * as uuid from 'uuid'
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
 export const setAlert = (
@@ -9,7 +9,8 @@ export const setAlert = (
   cart_id,
   timeout = 5000
 ) => (dispatch) => {
-  const id = uuid.v4();
+  // const id = uuid.v4();
+  const id = Math.floor(Math.random() * 1000000000);
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertStyle, alertType, id, line_item_id, cart_id },
